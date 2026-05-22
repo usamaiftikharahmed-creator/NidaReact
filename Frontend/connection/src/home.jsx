3567812ce29746b1b717f1be0aa3b3142bbafa14
usamaiftikharahmed-creator <usamaiftikharahmed@aptechnorth.edu.pk>
2026-05-22 18:01:18 +0500

    Update home.jsx

--- Frontend/connection/src/home.jsx
+++ Frontend/connection/src/home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Navbar from "./Navbar";
+import Navbar from "./navbar";
 
 export default function Home() {
     const [products, setProducts] = useState([]);
