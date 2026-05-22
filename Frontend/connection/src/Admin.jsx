95dbf7923bdf907d57d00b6568ec58e70a70b889
usamaiftikharahmed-creator <usamaiftikharahmed@aptechnorth.edu.pk>
2026-05-22 18:01:35 +0500

    Update Admin.jsx

--- Frontend/connection/src/Admin.jsx
+++ Frontend/connection/src/Admin.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "./navbar";
 
 export default function Admin() {
     const navigate = useNavigate();
